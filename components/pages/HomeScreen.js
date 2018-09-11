4f1b13c9821c1f06e203ccc92a1315336b09d646
Martin <martingsorbo@gmail.com>
2018-09-11 15:56:24 +0200

    Fikset noe på homescreen

--- components/pages/HomeScreen.js
+++ components/pages/HomeScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default class HomeScreen extends React.Component {
-  static navigationOption = {
-      title: "Home"
-  };
+  static navigationOptions = {
+    title: 'Home',
+    headerRight: ( 
+      <View style={{flex: 1, paddingRight: 12, alignItems: 'center', justifyContent: 'center'}}>
+        <Ionicons name = 'md-create' size= {25} />
+      </View>
+      ),
+};
   render(){
       return(
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
             <Text>Home!</Text>
             <Button
             title='Go to details'
--- components/pages/ProfileScreen.js
+++ components/pages/ProfileScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default class DetailsScreen extends React.Component {
     static navigationOptions = {
-        title: "Profile"
+        title: "Profile",
+        headerRight: ( 
+            <Ionicons name = 'md-create' size= {25} />
+          ),
     };
 
     render() {
