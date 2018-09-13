a9846fd194d2a8f75bffbc652653b6c7e51f865b
Martin <martingsorbo@gmail.com>
2018-09-13 14:26:01 +0200

    Laget ListView i homescreen og testet med array

--- components/pages/HomeScreen.js
+++ components/pages/HomeScreen.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View, Alert, ListView, ScrollView, ImageBackground} f
 import {Container, Header, Left, Body, Content, Footer, FooterTab, Title, Right, Button} from 'native-base';
 import {Ionicons} from '@expo/vector-icons';
 
+var eventArray = ["Down på å se film hos meg ikveld?", "Raclette hos meg idag?", "Noen som game fortnite?", "Hoste vors idag!"];
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Quickevent',
@@ -33,21 +35,40 @@ export default class HomeScreen extends React.Component {
       </View>
       ),
 };
+
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.guid != r2.guid});
+    this.state = {
+      dataSource: dataSource.cloneWithRows(eventArray)
+    }
+  }
+
+    renderRow(rowData, sectionID, rowID) {
+      return (
+        <View style={{backgroundColor: 'white'}}>
+          <View style={{height: 90, width: '100%', borderBottomColor: '#143311', borderBottomWidth: 1.5}}>
+          <View style={{flex: 1, alignItems: 'flex-start', paddingTop: 10, width: 70}}>
+          <ImageBackground source={{uri: 'https://static.thenounproject.com/png/363633-200.png'}}
+            style={styles.image} ></ImageBackground>
+          </View>
+          <View style={{alignItems: 'flex-start', justifyContent: 'center', paddingLeft: 80, paddingBottom: 30}}>
+          <Text style ={{fontSize:22,fontWeight:'500'}}>Tobias Rognstad</Text>
+          <Text style={{fontSize: 17}} numberOfLines={1}>{rowData}</Text>
+          </View>
+          </View>
+
+
+        </View>
+      )
+       
+      
+    }
   render(){
       return(
         <View>
           <ScrollView style={{height: '100%', position: 'relative'}}>
-
-              <Header style={{height:70, width: '100%', position:'absolute'}}>
-                <Left style={{flex: 1, alignItems: 'flex-start', paddingBottom:45, width: 70}}>
-                
-                <ImageBackground source={{uri: 'https://static.thenounproject.com/png/363633-200.png'}}
-                        style={styles.image} ></ImageBackground>
-                </Left>
-                <Text style ={{fontSize:18,fontWeight:'500', height:'100%', position:'absolute', paddingTop:15, paddingRight:70}}>Tobias Rognstad</Text>
-                <Text style={{width:'80%',paddingLeft:7}}>Down på å se film hos meg i kveld?</Text>
-              </Header>
-           
+           <ListView dataSource={this.state.dataSource} renderRow={this.renderRow.bind(this)}></ListView>
           </ScrollView>
         </View>
       )
