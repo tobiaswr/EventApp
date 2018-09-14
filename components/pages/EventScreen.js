c53edf00cd17e0077f862559f62d0f577b6bfd79
Tobias Rognstad <tobiasrognstad@Tobias-MacBook-Pro.local>
2018-09-14 18:57:07 +0200

    Comment input event screen

--- components/pages/EventScreen.js
+++ components/pages/EventScreen.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View, Button, ImageBackground, Image, TextInput} from 
 
 import {Container, Header, Left, Body, Content, Footer, FooterTab, Title, Right, Input} from 'native-base';
 import {Ionicons, FontAwesome, Entypo, MaterialIcons} from '@expo/vector-icons';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export default class EventScreen extends React.Component {
     static navigationOptions = {
@@ -27,43 +28,61 @@ export default class EventScreen extends React.Component {
         const { navigation } = this.props;
         const event = navigation.getParam('rowData', '');
         return(
-            <View style={{flex:1}}>
-        <View style={{backgroundColor: 'white', shadowRadius: 3, shadowOpacity:0.3, shadowOffset: {width: 1, height: 0}, shadowColor: '#000000', elevation: 4,}}>
-          <View style={{height: 140, width: '100%', borderBottomColor: '#143311', borderBottomWidth: 1.5}}>
-            <View style={{flex: 1, alignItems: 'flex-start', paddingTop:5, width: 70}}>
-                <ImageBackground source={{uri: 'https://static.thenounproject.com/png/363633-200.png'}}
-            style={{width: 60, height: 60,}} ></ImageBackground>
-            </View>
-            <View style={{alignItems: 'flex-start', position: 'absolute', paddingLeft: 65, top: 15}}>
-                <Text style ={{fontSize:18,fontWeight:'500'}}>{event.user}</Text>
-              <View style={{position: 'absolute',paddingLeft: 65,  top: 25, flexDirection:'row'}}>
-            <View style={{paddingRight:10, flexDirection:'row'}}>
-                <MaterialIcons style={{paddingRight:0, fontSize:13}} name='event-available'></MaterialIcons>
-                <Text style={{fontSize:11}}>{event.attendees.length}</Text> 
-            </View>
-            <View style={{flexDirection:'row'}}>
-                <MaterialIcons style={{paddingRight:0, fontSize:13}} name='event-busy'></MaterialIcons>
-                <Text style={{fontSize:11}}>{event.decliners.length}</Text>
-            </View>
-              </View>
-            </View>
+        <KeyboardAwareScrollView resetScrollToCoords={{ x: 0, y: 0 }} contentContainerStyle={styles.container} scrollEnabled={false}> 
+            <View style={{backgroundColor: 'white', shadowRadius: 3, shadowOpacity:0.3, shadowOffset: {width: 1, height: 0}, shadowColor: '#000000', elevation: 4,}}>
+                <View style={{height: 140, width: '100%', borderBottomColor: '#143311', borderBottomWidth: 1.5}}>
+                    <View style={{flex: 1, alignItems: 'flex-start', paddingTop:5, width: 70}}>
+                        <ImageBackground source={{uri: 'https://static.thenounproject.com/png/363633-200.png'}}
+                        style={{width: 60, height: 60,}} ></ImageBackground>
+                    </View>
+                    <View style={{alignItems: 'flex-start', position: 'absolute', paddingLeft: 65, top: 15}}>
+                        <Text style ={{fontSize:18,fontWeight:'500'}}>{event.user}</Text>
+                        <View style={{position: 'absolute',paddingLeft: 65,  top: 25, flexDirection:'row'}}>
+                            <View style={{paddingRight:10, flexDirection:'row'}}>
+                                <MaterialIcons style={{paddingRight:0, fontSize:13}} name='event-available'></MaterialIcons>
+                                <Text style={{fontSize:11}}>{event.attendees.length}</Text> 
+                            </View>
+                            <View style={{flexDirection:'row'}}>
+                                <MaterialIcons style={{paddingRight:0, fontSize:13}} name='event-busy'></MaterialIcons>
+                                <Text style={{fontSize:11}}>{event.decliners.length}</Text>
+                            </View>
+                        </View>
+                    </View>
              
-            <View style={{height: 80, justifyContent: 'center'}}>
-                <Text style={{paddingLeft: 5, fontSize: 25}}>{event.eventDesc}</Text>
-            </View>
+                    <View style={{height: 80, justifyContent: 'center'}}>
+                        <Text style={{paddingLeft: 5, fontSize: 25}}>{event.eventDesc}</Text>
+                    </View>
 
-          </View>
+                </View>
 
-          <View style={{flexDirection: 'row', position: 'absolute', width: '100%'}}>
-            <View style={{position: 'absolute', right: 10, paddingTop: 20, flexDirection: 'row'}}>
-                <Entypo style={{paddingRight: 25, fontSize:35}} name='check' onPress = {() => alert('Ready to hang out!')}></Entypo>
-                <Entypo style={{paddingRight:5, fontSize:35}} name='circle-with-cross'></Entypo>
-            </View>
-          </View>
+                <View style={{flexDirection: 'row', position: 'absolute', width: '100%'}}>
+                    <View style={{position: 'absolute', right: 10, paddingTop: 20, flexDirection: 'row'}}>
+                        <Entypo style={{paddingRight: 25, fontSize:35}} name='check' onPress = {() => alert('Ready to hang out!')}></Entypo>
+                        <Entypo style={{paddingRight:5, fontSize:35}} name='circle-with-cross'></Entypo>
+                    </View>
+                </View>
           
-        </View>
-        <TextInput placeholder='What would you like to comment?' style={{alignSelf:'center', position:'absolute', bottom:10, borderColor: 'black', borderRadius:5}}></TextInput>
-        </View>
+            </View>
+            
+                <TextInput placeholder='What would you like to comment?' style={styles.input}></TextInput>
+        </KeyboardAwareScrollView>
+        
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'stretch',
+    },
+    input: {
+        height: 50,
+        width:'99%',
+        position:'absolute',
+        bottom:0,
+        alignSelf:'center',
+        borderRadius:18,
+        backgroundColor:'white'
+    },
+});
