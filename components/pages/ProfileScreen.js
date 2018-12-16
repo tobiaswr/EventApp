db0f37358640f974fef77b5e64ff0797e6e805b5
Martin <martingsorbo@gmail.com>
2018-12-16 18:54:49 +0100

    Made a simple profile screen, only issue is to figure out how to get only the users events see line 88

--- components/pages/ProfileScreen.js
+++ components/pages/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ImageBackground, ScrollView} from 'react-native';
+import ItemComponent from './ItemComponent';
 
 export default class ProfileScreen extends Component{
     static navigationOptions = ({navigation}) => ({
@@ -25,7 +26,8 @@ export default class ProfileScreen extends Component{
         this.state = {
           users: [],
           username: '',
-          uid: firebase.auth().currentUser.uid
+          events: [],
+          myEvents: [],
         };
         
         
@@ -37,6 +39,42 @@ export default class ProfileScreen extends Component{
             let users = Object.values(data);
             this.setState({users})            
         });
+
+        firebase.database().ref('/events').on('value', (snapshot) => {
+            if(snapshot.val() != null){
+              let data = snapshot.val();
+              let eventkeys = Object.keys(data);
+              let events = Object.values(data);
+              let comments = [];
+              let i = 0;
+              events.forEach(event => {
+                event.id = eventkeys[i];
+                firebase.database().ref('/events/' + event.id + '/comments').on('value', (snapshot) => {
+                  let data = snapshot.val();
+                  comments = Object.values(data);
+                  const result = comments.filter(comment => comment.commentText.length >0);
+                  comments = result;
+  
+                });
+                firebase.database().ref('/events/' + event.id + '/attendees').on('value', (snapshot) => {
+                  let data = snapshot.val();
+                  attendees = Object.values(data);
+                });
+                firebase.database().ref('/events/' + event.id + '/decliners').on('value', (snapshot) => {
+                  let data = snapshot.val();
+                  decliners = Object.values(data);
+                  const filter = decliners.filter(decliner => decliner.username.length > 0);
+                  decliners = filter;
+                });
+                event.comments = comments;
+                event.attendees = attendees;
+                event.decliners = decliners;
+                i++;
+              })
+              this.setState({events});
+            }
+           });
+      
     }
 
     render() {
@@ -45,12 +83,28 @@ export default class ProfileScreen extends Component{
                 this.state.username= user.username;
             }
         });
-        const username = this.state.username;
-        console.log(username);
+        let events = [];
+        this.state.events.forEach(event => {
+            if(event.owner === 'martings'){
+                this.state.myEvents.push(event);
+            }
+        })
         return(
-            <View>
-                <Text>{this.state.username}</Text>
-                <Text>{this.state.uid}</Text>
+            <View style={{flex: 1, justifyContent: 'center'}}>
+                <View style={{height: '20%', backgroundColor: 'white', flexDirection: 'row'}}>
+                <ImageBackground source={{uri: 'https://static.thenounproject.com/png/363633-200.png'}}
+                style={{width: 75, height: 75, top: 10, left: 10}}
+                ></ImageBackground>
+                <Text style={{fontSize: 30, top: 20, left: 20}}>{this.state.username}</Text>
+
+
+                </View>
+                <View style={{height: 25, backgroundColor: 'white', borderBottomWidth: 0.5, borderColor: '#22561e'}}>
+                <Text style={{fontSize: 25,}}>My events:</Text>
+                </View>
+                <ScrollView >
+                    ? <ItemComponent navigation={this.props.navigation} events={this.state.myEvents} style={{width: '100%'}}/>  
+                </ScrollView>
             </View>            
         );
     }
